44b8398d26758ba8dd05c479ec558a02a1e89479
mars.hung <tfaredxj@gamil.com>
2019-08-17 16:35:30 +0800

    [Dev] First program: Hello World!

---
+++ application/views/hello.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello World!</title>
+</head>
+<body>
+
+	<h1>Hello World!</h1>
+
+</body>
+</html>
